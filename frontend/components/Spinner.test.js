da56cfcd2b179171626b6b64db31bb04993fcd06
JaceRogers12 <jacerogers12@gmail.com>
2024-04-11 14:16:12 -0600

    made spinner test

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,13 @@
+import Spinner from "./Spinner.js";
+import { render, screen } from "@testing-library/react"; 
+import React from "react";
+
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
 test('sanity', () => {
-  expect(true).toBe(false)
+  const {rerender} = render(<Spinner on={false} />)
+  expect(screen.queryByText("Please wait...")).not.toBeVisible;
+  
+  rerender(<Spinner on={true} />)
+  expect(screen.queryByText("Please wait...")).toBeVisible;
 })
